ae8cdaf7f7b367023b74c50d1f6bef9c4b5d2337
KarasovRuslan <karasov.ruslan@icloud.com>
2022-02-20 02:13:32 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -6,8 +6,3 @@ let teacher = new Teacher('Oleg', 33, 'Male', 'IT', 'Front-end Pro');
 
 console.log('Student', student);
 console.log('Teacher', teacher);
-
-const heading = document.createElement('h1')
-heading.textContent = 'Привет Вебпак'
-const root = document.querySelector('#root')
-root.append(heading);
